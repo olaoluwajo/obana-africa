a3d185fab8ddcb16151230da4b835865e8fa9f6a
olaoluwajo <olagundoyealexander@gmail.com>
2024-12-11 16:44:53 +0100

    add product build

--- src/app/api/(image)/uploadImage/route.ts
+++ src/app/api/(image)/uploadImage/route.ts
@@ -7,6 +7,13 @@ cloudinary.config({
 	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+console.log("Cloudinary Config:", {
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY ? "Loaded" : "Not Loaded",
+	api_secret: process.env.CLOUDINARY_API_SECRET ? "Loaded" : "Not Loaded",
+});
+
+
 // export const config = {
 // 	api: {
 // 		bodyParser: false,
