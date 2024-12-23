fc7cc2f67eb17675bf6bb5cc87f6b031f4965311
olaoluwajo <olagundoyealexander@gmail.com>
2024-12-23 18:34:22 +0100

    review edit

--- src/app/vendor/dashboard/product/_components/product-actions.tsx
+++ src/app/vendor/dashboard/product/_components/product-actions.tsx
@@ -13,6 +13,14 @@ export default function ProductActions({ productId }: { productId: string }) {
 		console.log(`Redirecting to edit page for product ID: ${productId}`);
 		router.push(`/vendor/dashboard/product/${productId}`);
 	};
+	const handleDuplicate = () => {
+		if (!productId) {
+			console.error("Product ID is required.");
+			return;
+		}
+		console.log(`Redirecting to edit page for product ID: ${productId}`);
+		router.push(`/vendor/dashboard/product/${productId}?duplicate=true`);
+	};
 
 	const handleDelete = () => {
 		const confirmed = confirm("Are you sure you want to delete this product?");
@@ -30,7 +38,7 @@ export default function ProductActions({ productId }: { productId: string }) {
 				Edit Product
 			</button>
 			<button
-				onClick={handleEdit}
+				onClick={handleDuplicate}
 				className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition">
 				Duplicate
 			</button>
